3b4bd59a177a53a65a6960e7f849ff6c8ce0e37d
wzr <wangzhiru1@huawei.com>
2024-03-28 17:21:35 +0800

    TA compile adapt.
    Signed-off-by: wzr <wangzhiru1@huawei.com>

---
+++ sdk/build/mk/common_gcc.mk
@@ -0,0 +1,21 @@
+# Copyright (C) 2022 Huawei Technologies Co., Ltd.
+# Licensed under the Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan
+# PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#     http://license.coscl.org.cn/MulanPSL2
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+# KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+# NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more details.
+
+# set gcc compiler
+ifneq ($(TARGET_IS_ARM64), y)
+    CC = arm-eabi-gcc
+    LD = arm-eabi-ld
+    READELF = arm-eabi-readelf
+else
+    CC = aarch64-linux-gnu-gcc
+    LD = aarch64-linux-gnu-ld
+    READELF = aarch64-linux-gnu-readelf
+endif
